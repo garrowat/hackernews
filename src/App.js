fbf7a58e50867c6668fa8046b760039c397f1da8
GW <garrettbauer@gmail.com>
2017-11-26 16:37:12 +0900

    Switching dev stations

--- src/App.js
+++ src/App.js
@@ -18,6 +18,30 @@ const list = [
     points: 5,
     objectID: 1,
   },
+  {
+    title: 'Flux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 2,
+  },
+  {
+    title: 'Inferno',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 5,
+    points: 5,
+    objectID: 3,
+  },
+  {
+    title: 'Meteor',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 3,
+    points: 5,
+    objectID: 4,
+  },
 ];
 
 class App extends Component {
@@ -27,16 +51,34 @@ class App extends Component {
     this.state = {
       list,
       fontSize: 10,
+      color: '#000000',
     };
-
+    this.increaseFontSize = this.increaseFontSize.bind(this);
+    this.resetFontSize = this.increaseFontSize.bind(this);
+    this.randomColor = this.increaseFontSize.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
 
-  increaseFontSize = () => {
+  increaseFontSize() {
     this.setState({fontSize: this.state.fontSize * 2});
   }
 
-  resetFontSize = () => {
+  resetFontSize() {
     this.setState({fontSize: 10});
+  }
+
+  randomColor() {
+    const randomHex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    this.setState({color: randomHex});
+  }
+
+  onDismiss(id) {
+    const isNotID = (item) => {
+      return item.ObjectID !== id;
+    }
+
+    const updatedList = {this.state.list.filter(isNotId)};
+    this.setState({list: updatedList});
   }
 
   render() {
@@ -47,10 +89,12 @@ class App extends Component {
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span style={{fontSize: this.state.fontSize, transition: '0.5s',}}>{item.author}</span>
+            <span style={{color: this.state.color, fontSize: this.state.fontSize, transition: '0.5s',}}>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <button onClick={() => this.onDismiss(item.ObjectID)}>Dismiss</button>
             <button onClick={this.increaseFontSize}>Current Size: { this.state.fontSize }</button>
+            <button onClick={this.randomColor}>Current Color: { this.state.color }</button>
             <button onClick={this.resetFontSize}>Reset</button>
           </div>
         )}
