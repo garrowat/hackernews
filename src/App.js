8a29f2d38afa055a05a0b8ad36fa86013f45099c
GW <garrettbauer@gmail.com>
2017-11-29 01:11:46 +0900

    COmpleted stage one of book, added styling and FSCs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+require('./App.css');
 
 const list = [
   {
@@ -9,10 +9,6 @@ const list = [
     num_comments: 3,
     points: 4,
     objectID: 0,
-    style: {
-      color: '#000000',
-      fontSize: 10,
-    }
   },
   {
     title: 'Redux',
@@ -21,46 +17,22 @@ const list = [
     num_comments: 2,
     points: 5,
     objectID: 1,
-    style: {
-      color: '#000000',
-      fontSize: 10,
-    }
   },
   {
-    title: 'Flux',
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 2,
+  },
+  {
+    title: 'Redux',
     url: 'https://github.com/reactjs/redux',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
-    objectID: 2,
-    style: {
-      color: '#000000',
-      fontSize: 10,
-    }
-  },
-  {
-    title: 'Inferno',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 5,
-    points: 5,
     objectID: 3,
-    style: {
-      color: '#000000',
-      fontSize: 10,
-    }
-  },
-  {
-    title: 'Meteor',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 3,
-    points: 5,
-    objectID: 4,
-    style: {
-      color: '#000000',
-      fontSize: 10,
-    }
   },
 ];
 
@@ -68,6 +40,7 @@ const isSearched = (searchTerm) => (item) =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
+
   constructor(props) {
     super(props);
 
@@ -75,85 +48,85 @@ class App extends Component {
       list,
       searchTerm: '',
     };
+
   }
 
   onSearchChange = (event) => {
     this.setState({ searchTerm: event.target.value });
   }
 
-  increaseFontSize = (id) => {
-    const updatedList = this.state.list.reduce((itemList, item) => {
-      if (item.objectID === id) {
-        item.style.fontSize *= 2;
-      }
-      itemList.push(item);
-      return itemList;
-    },[]);
-    this.setState({list: updatedList});
-  }
-
-  resetFontSize = (id) => {
-    const updatedList = this.state.list.reduce((itemList, item) => {
-      if (item.objectID === id) {
-        item.style.fontSize = 10;
-      }
-      itemList.push(item);
-      return itemList;
-    },[]);
-    this.setState({list: updatedList});
-  }
-
-  onDismiss(id) {
+  onDismiss = (id) => {
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
-    this.setState({list: updatedList});
-  }
-
-  randomColor = (id) => {
-    const randomHex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    const updatedList = this.state.list.reduce((itemList, item) => {
-      if (item.objectID === id) {
-        item.style.color = randomHex;
-      }
-      itemList.push(item);
-      return itemList;
-    },[]);
-    this.setState({list: updatedList});
+    this.setState({ list: updatedList });
   }
 
   render() {
-    const { searchTerm, List } = this.state;
+    const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        />
-        <form>
-          <input
-          type='text'
-          value={searchTerm}
-          onChange={this.onSearchChange} />
-        </form>
-
-        { list.filter(isSearched(searchTerm)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span style={{color: item.style.color, fontSize: item.style.fontSize, transition: '0.5s',}}>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <button onClick={() => this.onDismiss(item.objectID)} type='button'>Dismiss</button>
-              <button onClick={() => this.increaseFontSize(item.objectID)}>Current Size: { item.style.fontSize }</button>
-              <button onClick={() => this.randomColor(item.objectID)}>Current Color: { item.style.color }</button>
-              <button onClick={() => this.resetFontSize(item.objectID)}>Reset</button>
-            </div>
-          )
-        }
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+          Search
+          </Search>
+          <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        </div>
       </div>
     );
   }
 }
+
+const Search = ({value, onChange, children}) =>
+  <form>
+    {children}<input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+const Table = ({list, pattern, onDismiss}) =>
+  <div className='table'>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div className='table-row' key={item.objectID}>
+        <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>
+          {item.author}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.num_comments}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.points}
+        </span>
+        <span style={{ width: '10%' }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
+const Button = ({onClick, className = '', children,}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type='button'
+  >
+    {children}
+  </button>
 
 export default App;
