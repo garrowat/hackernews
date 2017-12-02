95045de2164bee3928b748b6d1518baf3adc8dbb
GW <garrettbauer@gmail.com>
2017-12-03 01:06:19 +0900

    Added API fetching, continuous pagination

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100'
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const isSearched = (searchTerm) => (item) =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -22,62 +25,103 @@ class App extends Component {
 
   }
 
-  setSearchTopStories = result =>
-    this.setState({result});
+  setSearchTopStories = (result) => {
+    const {hits, page} = result;
 
-  fetchSearchTopStories = (searchTerm) => {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    const oldHits = page !== 0
+    ? this.state.result.hits
+    : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      result: {hits: updatedHits, page}
+    });
+  }
+
+  fetchSearchTopStories = (searchTerm, page = 0) => {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(e => e);
   }
 
-  component
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+  }
 
   onSearchChange = (event) => {
     this.setState({ searchTerm: event.target.value });
   }
 
+  onSearchSubmit = (event) => {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
+
   onDismiss = (id) => {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: {...this.state.result, hits: updatedHits }
+    });
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
     return (
       <div className="page">
         <div className="interactions">
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
           Search
           </Search>
-          <Table
-            list={list}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
+          { result &&
+            <Table
+              list={result.hits}
+              onDismiss={this.onDismiss}
+            />
+          }
+          <div className='interactions'>
+            <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+              More
+            </Button>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-const Search = ({value, onChange, children}) =>
-  <form>
+const Search = ({
+  value,
+  onChange,
+  children,
+  onSubmit,
+}) =>
+  <form onSubmit={onSubmit}>
     {children}<input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <Button type='submit'>
+      {children}
+    </Button>
   </form>
 
-const Table = ({list, pattern, onDismiss}) =>
+const Table = ({list, onDismiss}) =>
   <div className='table'>
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div className='table-row' key={item.objectID}>
         <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
